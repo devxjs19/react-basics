1f815e5b31850a99f311f0b48fdabc3230ff7458
Jake Samuels <samuels242@googlemail.com>
2025-08-10 21:16:38 +0100

    Import ImageDisplay

--- simple-image-carousel/src/App.jsx
+++ simple-image-carousel/src/App.jsx
@@ -1,7 +1,11 @@
+import ImageDisplay from "./components/ImageDisplay";
+
 const App = () => {
   return (
     <div>
       <h1>Simple Image Carousel</h1>
+
+      <ImageDisplay />
     </div>
   );
 };
