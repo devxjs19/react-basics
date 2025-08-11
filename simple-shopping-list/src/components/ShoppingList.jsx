b60aa85cb18b5d4b271563e51d048e44b38ed26b
Jake Samuels <samuels242@googlemail.com>
2025-08-11 17:27:26 +0100

    Export components to own files

---
+++ simple-shopping-list/src/components/ShoppingList.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+import ListInput from "./ListInput";
+import ListItems from "./ListItems";
+
+const ShoppingList = () => {
+  const [listItems, setListItems] = useState(() => {
+    const saved = localStorage.getItem("listItems");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("listItems", JSON.stringify(listItems));
+  }, [listItems]);
+
+  const handleAddItem = (newItem) => {
+    const updatedList = [...listItems, newItem];
+    setListItems(updatedList);
+    localStorage.setItem("listItems", JSON.stringify(updatedList));
+  };
+
+  const handleDeleteItem = (item) => {
+    const updatedList = listItems.filter((el) => el !== item);
+    setListItems(updatedList);
+    localStorage.setItem("listItems", JSON.stringify(updatedList));
+  };
+
+  return (
+    <div className="shopping-list">
+      <ListInput listItems={listItems} onAddItem={handleAddItem} />
+      <ListItems listItems={listItems} onDeleteItem={handleDeleteItem} />
+    </div>
+  );
+};
+
+export default ShoppingList;
