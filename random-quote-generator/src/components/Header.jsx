95bfad0d15d36ae6af864bb82eab99e2b49a1813
Jake Samuels <samuels242@googlemail.com>
2025-08-13 00:10:15 +0100

    Header

---
+++ random-quote-generator/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { BsChatLeftQuoteFill } from "react-icons/bs";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <BsChatLeftQuoteFill />
+        Random Quotes...
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
